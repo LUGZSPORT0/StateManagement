b53ab265de9c133337ee9691e4f321210db600bd
LUGZSPORT0 <arjean85@gmail.com>
2023-03-26 00:02:17 -0400

    push changes from cartstore

--- pinia-lesson/src/stores/CartStore.js
+++ pinia-lesson/src/stores/CartStore.js
@@ -9,7 +9,7 @@ export const useCartStore = defineStore("CartStore", {
         addItems(count, item) {
             count = parseInt(count);
             for(let index= 0;index < count;index++){
-                this.items.push(item)
+                this.items.push({...item}) // spread item into its own object,effectively cloning it
             }
         }
     }
