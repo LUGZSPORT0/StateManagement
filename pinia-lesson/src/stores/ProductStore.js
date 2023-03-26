806cdc585931b0c2ae68240cab0e69ec691675c7
LUGZSPORT0 <arjean85@gmail.com>
2023-03-25 22:54:15 -0400

    set up the empty global state and a fill action in the store; then use the fill action in the main app.

--- pinia-lesson/src/stores/ProductStore.js
+++ pinia-lesson/src/stores/ProductStore.js
@@ -1,12 +1,15 @@
 import { defineStore } from "pinia";
-import products from '@/data/products.json'
 export const useProductStore = defineStore("ProductStore", { // necessary but needs to be unique
     state: () => { // state central part of the store
         return {
-            products, // something that we are storing in state
+            products: [], // something that we are storing in state in an empty global state
         }
-    }
-    // actions
+    },
+    actions: { // actions
+        async fill() {
+            this.products = (await import("@/data/products.json")).default; // fills the global state
+        },
+    },
     // getters
 }) 
 
