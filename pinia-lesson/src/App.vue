e35cff32569c5c43b6cc6d08bd1a2c3a44de5949
LUGZSPORT0 <arjean85@gmail.com>
2023-03-26 00:01:29 -0400

    added a cart store that updates the cart (in console) and made sure items are not replicated objects

--- pinia-lesson/src/App.vue
+++ pinia-lesson/src/App.vue
@@ -2,10 +2,26 @@
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from '@/stores/ProductStore';
+import { useCartStore } from '@/stores/CartStore';
 import {storeToRefs} from 'pinia'
 const productStore = useProductStore();
+const cartStore = useCartStore();
 // const {products} = storeToRefs(useProductStore()); //destructed version
 productStore.fill();
+//// the addToCart method is no longer in use since we added an action to CartStore to do this logic for us
+//// with benefit of no needed patch or separate items in timeline - addItems start and addItems end 
+// const addToCart = (count, product)=> {
+//   count = parseInt(count)
+//     for(let index= 0;index < count;index++){
+//     state.items.push(product)  
+//     }
+//   //// patch to combine the items together
+//   // cartStore.$patch(state=> {
+//   //   for(let index= 0;index < count;index++){
+//   //   state.items.push(product)
+//   //   }
+//   // })
+// // }
 </script>
 
 <template>
@@ -16,7 +32,9 @@ productStore.fill();
         v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+        @addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
 </template>
+<!-- @addToCart="addToCart($event, product)" when either using patch or simple for loop -->
